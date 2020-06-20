69ab05435f6f400b6b708fbc31cdc3977aad1f0b
Shane Jennings <cshanejennings@gmail.com>
2020-06-20 18:17:11 -0400

    react: created client test harness

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
+axios.defaults.headers.common = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+};
+
+const URI_BASE = 'http://localhost:8000';
+
+const ENDPOINT = {
+  // not sure if this is necessary
+  csrf: `${URI_BASE}/sanctum/csrf-cookie`,
+  login: `${URI_BASE}/api/login`,
+  user: `${URI_BASE}/api/user`,
+}
+
+axios.get(ENDPOINT.csrf, {
+  'Access-Control-Allow-Origin': '*',
+}).then(response=>{
+  // ? not sure if this is needed
+});
+
+
 function App() {
+  const [ email, set_email ] = React.useState('admin@test.com');
+  const [ password, set_password ] = React.useState('12345678');
+
+  const [ token, set_token ] = React.useState('');
+
+  const update_email = e => set_email(e.target.value);
+  const update_pw = e => set_password(e.target.value);
+
+  const test_credentials = () => {
+    axios.post(ENDPOINT.login, {email, password}, {
+      'Access-Control-Allow-Origin': '*',
+    }).then(response => {
+      const tkn = response.data; // wherever the token is stored
+      set_token(tkn);
+    });
+  }
+
+  const test_token = () => {
+    axios.get(ENDPOINT.user, {
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': `Bearer ${token}`,
+    }).then(response => {
+      const tkn = response.data;
+      set_token(tkn);
+    });
+  }
+
+  const form_area = (!token) ? (
+    <Fragment>
+      <input className="form-text" onChange={ update_email } value={ email } type="text"/>
+      <input className="form-text" onChange={ update_pw } value= { password } type="text"/>
+      <button className="submit-btn" onClick={ test_credentials }>Test Credentials</button>
+    </Fragment>
+  ) : (
+    <Fragment>
+      <h4>Token</h4>
+      <input className="form-text" type="text" value={ token } onChange={ () => {} }/>
+      <p>Test the { ENDPOINT.user } webservice.</p>
+      <button className="submit-btn" onClick={ test_token }>Load User Data</button>
+    </Fragment>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Login test harness</header>
+      <hr/>
+      { form_area }
     </div>
   );
 }
